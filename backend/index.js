90d22a0395adeecb3635813f86d93f07a7c01100
Shubham Maurya <146547511+shubhammaurya0221@users.noreply.github.com>
2025-10-13 20:16:05 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,8 +7,7 @@ import cors from "cors"
 import userRouter from "./routes/userRoute.js"
 import courseRouter from "./routes/courseRoute.js"
 
-// import aiRouter from "./routes/aiRoute.js"
-import reviewRouter from "./routes/reviewRoute.js"
+
 import testRouter from "./routes/testRoute.js"
 import mcqRouter from "./routes/mcqRoute.js"
 import noteRouter from "./routes/noteRoute.js"
@@ -27,22 +26,12 @@ let port = process.env.PORT
 let app = express()
 app.use(express.json())
 app.use(cookieParser())
-const allowedOrigins = [
-    "https://indraprasth-demo-frontend.onrender.com",
-    "http://localhost:5173" // add your dev frontend URL
-];
-
 app.use(cors({
-    origin: function(origin, callback) {
-        if (!origin) return callback(null, true); // allow non-browser requests
-        if (allowedOrigins.indexOf(origin) === -1) {
-            return callback(new Error("CORS policy does not allow access from this origin"), false);
-        }
-        return callback(null, true);
-    },
+    origin: process.env.NODE_ENV === 'production' 
+        ? [process.env.FRONTEND_URL, "https://lms-frontend.vercel.app"]
+        : ["http://localhost:5173", "http://localhost:5174", "http://localhost:5175","https://indraprashta-frontend.onrender.com"],
     credentials: true
-}));
-
+}))
 // Serve static files from uploads directory
 app.use('/uploads', express.static(path.join(__dirname, 'public', 'uploads')))
 
@@ -51,7 +40,7 @@ app.use("/api/user", userRouter)
 app.use("/api/course", courseRouter)
 
 // app.use("/api/ai", aiRouter)
-app.use("/api/review", reviewRouter)
+
 app.use("/api/test-series", testRouter)
 app.use("/api/mcq", mcqRouter)
 app.use("/api/notes", noteRouter)
