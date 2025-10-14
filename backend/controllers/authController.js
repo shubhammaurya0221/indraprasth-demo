8fddff7af1e387a132f9c4990d80e68d89b2d59f
Shubham Maurya <146547511+shubhammaurya0221@users.noreply.github.com>
2025-10-15 00:31:03 +0530

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -95,7 +95,7 @@ export const login = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
+      secure: true,
       sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
